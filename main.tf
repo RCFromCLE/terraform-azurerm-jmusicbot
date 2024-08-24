2a1702c75e2fd0b0cae60e11c1a6b904c2c1b0cc
Rudy <rudycorradetti4@gmail.com>
2024-08-24 07:23:15 -0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 # author: Rudy Corradetti
-############################################ terraform and provider blocks ############################################
 terraform {
   required_version = ">=0.12"
   required_providers {
@@ -11,55 +10,76 @@ terraform {
       source  = "hashicorp/tls"
       version = "~>4.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = var.sub
 }
 
-############################################ data sources ############################################
-# data "azurerm_key_vault" "jdiscord_kv" {
-#   name                = var.key_vault_name
-#   resource_group_name = var.key_vault_resource_group_name
-# }
+# Data source to fetch latest release information
+data "http" "latest_release" {
+  url = "https://api.github.com/repos/jagrosh/MusicBot/releases/latest"
+  request_headers = {
+    Accept = "application/vnd.github.v3+json"
+  }
+}
 
-# data "azurerm_key_vault_secret" "ssh_public_key" {
-#   name         = "ssh-public-key"
-#   key_vault_id = data.azurerm_key_vault.jdiscord_kv.id
-# }
+locals {
+  latest_version = jsondecode(data.http.latest_release.body).tag_name
+  jar_filename   = "JMusicBot-${local.latest_version}.jar"
+  download_url   = "https://github.com/jagrosh/MusicBot/releases/download/${local.latest_version}/${local.jar_filename}"
+}
 
-# locals {
-#   ssh_public_key = trimspace(data.azurerm_key_vault_secret.ssh_public_key.value)
-# }
-############################################ resource blocks ############################################
-# create a resource group
+# Create a random string for the storage account name
+resource "random_string" "sa_suffix" {
+  length  = 5
+  special = false
+  upper   = false
+  numeric = true
+  lower   = true
+}
+
+# Resource group
 resource "azurerm_resource_group" "rg1" {
   name     = var.rg
   location = var.rg_loc
 }
-# create virtual network
+
+# Virtual network
 resource "azurerm_virtual_network" "vnet1" {
   name                = var.net
   address_space       = ["10.0.0.0/23"]
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
 }
-# create subnet
+
+# Subnet
 resource "azurerm_subnet" "subnet1" {
   name                 = var.subnet
   resource_group_name  = azurerm_resource_group.rg1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-# create public ips
+
+# Public IP
 resource "azurerm_public_ip" "public_ip" {
   name                = var.pub_ip
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
   allocation_method   = var.pub_allocation_method
 }
-# create network security group and rule
+
+# Network security group
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsg
   location            = azurerm_resource_group.rg1.location
@@ -77,7 +97,8 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
-# create network interface
+
+# Network interface
 resource "azurerm_network_interface" "nic" {
   name                = var.nic_name
   location            = azurerm_resource_group.rg1.location
@@ -90,17 +111,20 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
-# connect the security group to the network interface
+
+# Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "nsg_nic_assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
-# create a private key for the virtual machine
+
+# SSH key
 resource "tls_private_key" "linux_test_ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-# create a virtual machine run jdiscordbot service
+
+# Virtual machine
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                            = var.vm_name
   location                        = azurerm_resource_group.rg1.location
@@ -127,39 +151,26 @@ resource "azurerm_linux_virtual_machine" "vm1" {
     sku       = var.vm_image_sku
     version   = var.vm_image_version
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   lifecycle {
     ignore_changes = [boot_diagnostics]
   }
 }
-data "azurerm_public_ip" "vm_ip" {
-  name                = azurerm_public_ip.public_ip.name
-  resource_group_name = azurerm_resource_group.rg1.name
-}
-# Data source to fetch latest release information
-data "http" "latest_release" {
-  url = "https://api.github.com/repos/jagrosh/MusicBot/releases/latest"
 
-  request_headers = {
-    Accept = "application/vnd.github.v3+json"
-  }
-}
-
-locals {
-  latest_version = jsondecode(data.http.latest_release.body).tag_name
-  jar_filename   = "JMusicBot-${local.latest_version}.jar"
-  download_url   = "https://github.com/jagrosh/MusicBot/releases/download/${local.latest_version}/${local.jar_filename}"
-}
-
-# Create a storage account
+# Storage account for JMusicBot
 resource "azurerm_storage_account" "jmusicbot_storage" {
-  name                     = "jmusicbotstorage${random_string.suffix.result}"
+  name                     = "jmusicbot${random_string.sa_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Create a container in the storage account
+# Container in the storage account
 resource "azurerm_storage_container" "jmusicbot_container" {
   name                  = "jmusicbot-files"
   storage_account_name  = azurerm_storage_account.jmusicbot_storage.name
@@ -173,6 +184,13 @@ resource "azurerm_storage_blob" "jmusicbot_jar" {
   storage_container_name = azurerm_storage_container.jmusicbot_container.name
   type                   = "Block"
   source_uri             = local.download_url
+}
+
+# Role assignment for VM to access storage
+resource "azurerm_role_assignment" "vm_storage_blob_reader" {
+  scope                = azurerm_storage_account.jmusicbot_storage.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = azurerm_linux_virtual_machine.vm1.identity[0].principal_id
 }
 
 # VM extension to set up JMusicBot
@@ -200,6 +218,9 @@ sudo apt-get install -y default-jre
 # Create directory for JMusicBot
 sudo mkdir -p /opt/jmusicbot
 cd /opt/jmusicbot
+
+# Use managed identity to authenticate Azure CLI
+az login --identity
 
 # Download JAR file from Azure Storage
 az storage blob download --account-name ${azurerm_storage_account.jmusicbot_storage.name} \
@@ -232,6 +253,11 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
+# Set proper permissions
+sudo chown -R nobody:nogroup /opt/jmusicbot
+sudo chmod 644 /opt/jmusicbot/${local.jar_filename}
+sudo chmod 644 /opt/jmusicbot/config.txt
+
 # Reload systemd, enable and start the service
 sudo systemctl daemon-reload
 sudo systemctl enable jmusicbot.service
@@ -244,45 +270,43 @@ EOT
 
   depends_on = [
     azurerm_storage_blob.jmusicbot_jar,
-    azurerm_linux_virtual_machine.vm1
+    azurerm_linux_virtual_machine.vm1,
+    azurerm_role_assignment.vm_storage_blob_reader
   ]
 }
-# Create a random string for the storage account name
-resource "random_string" "sa_suffix" {
-  length  = 5 # Adjusted length for the suffix
-  special = false
-  upper   = false
-  numeric = true
-  lower   = true # Ensure lowercase is explicitly stated, though it's the default
-}
-# storage account for function app
+
+# Storage account for function app
 resource "azurerm_storage_account" "functionapp_sa" {
-  name                     = "jdiscordstorage${random_string.sa_suffix.result}"
+  name                     = "jdiscord${random_string.sa_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg1.name
   location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
 resource "azurerm_storage_container" "functionapp_container" {
   name                  = "jdiscord-code"
   storage_account_name  = azurerm_storage_account.functionapp_sa.name
   container_access_type = "private"
 }
-# Linux app service plan.
+
+# Linux app service plan
 resource "azurerm_service_plan" "functionapp_plan" {
   name                = "jdiscord-app-service-plan"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
   os_type             = "Linux"
-  sku_name            = "Y1" # "Y1" is the SKU for the Consumption plan.
+  sku_name            = "Y1"
 }
-# Application insights for monitoring.
+
+# Application insights for monitoring
 resource "azurerm_application_insights" "app_insights" {
   name                = "jdiscord-appinsights"
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
   application_type    = "web"
 }
+
 # Linux function app
 resource "azurerm_linux_function_app" "jdiscord_function" {
   name                       = "jdiscord-function"
@@ -291,25 +315,28 @@ resource "azurerm_linux_function_app" "jdiscord_function" {
   service_plan_id            = azurerm_service_plan.functionapp_plan.id
   storage_account_name       = azurerm_storage_account.functionapp_sa.name
   storage_account_access_key = azurerm_storage_account.functionapp_sa.primary_access_key
+  
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"       = "node"                 # This is the runtime for the function app, do not change this unless you know what you're doing.
-    "GENERAL_CHANNEL_ID"             = var.general_channel_id # this is the channel id for the general channel where the music bot will send status updates
-    "AFK_CHANNEL_ID"                 = var.afk_channel_id     # this is the channel id for the afk channel
-    "MUSIC_CHANNEL_ID"               = var.music_channel_id   # this is the channel id for the music bot channel
-    "DISCORD_BOT_TOKEN"              = var.discord_bot_token  # this is the bot token
+    "FUNCTIONS_WORKER_RUNTIME"       = "node"
+    "GENERAL_CHANNEL_ID"             = var.general_channel_id
+    "AFK_CHANNEL_ID"                 = var.afk_channel_id
+    "MUSIC_CHANNEL_ID"               = var.music_channel_id
+    "DISCORD_BOT_TOKEN"              = var.discord_bot_token
     "AZURE_TENANT_ID"                = var.azure_tenant_id
-    "AZURE_CLIENT_ID"                = var.azure_client_id # this is the client id of the service principal - grant sp access to the resource group or subscription to reboot the vm
+    "AZURE_CLIENT_ID"                = var.azure_client_id
     "AZURE_CLIENT_SECRET"            = var.azure_client_secret
     "SUBSCRIPTION_ID"                = var.sub
     "RESOURCE_GROUP_NAME"            = azurerm_resource_group.rg1.name
-    "VM_NAME"                        = var.vm_name # Assuming you have this defined elsewhere or passed as a variable.
+    "VM_NAME"                        = var.vm_name
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insights.instrumentation_key
   }
+  
   site_config {
     application_stack {
-      node_version = "18" # This is the version of node that the function app will use. Do not change this unless you know what you're doing.
+      node_version = "18"
     }
   }
+  
   lifecycle {
     ignore_changes = [
       app_settings["APPINSIGHTS_INSTRUMENTATIONKEY"],
